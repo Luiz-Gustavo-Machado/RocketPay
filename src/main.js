1a7b1195cbfe55d53dfb1f0a0fad5fbca4fe0718
Luiz Gustavo Machado <luiz.gustavo.machado@hotmail.com>
2022-10-18 15:02:24 -0300

    file colores

--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
 import "./css/index.css"
+
+const ccbgcolor01 = document.querySelector('.cc-bg svg > g g:nth-child(1) path')
+const ccbgcolor02 = document.querySelector('.cc-bg svg > g g:nth-child(2) path')
+const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
+
+
+function setCardType(type) {
+  const colors = {
+  "visa": ["#436D99", "#2D57F2"],
+  "MasterCard": ["#C69347", "#DF6F29"],
+  "default": ["black", "gray"],
+
+}
+
+  ccbgcolor01.setAttribute('fill', colors[type] [0])
+  ccbgcolor02.setAttribute('fill', colors[type] [1])
+  ccLogo.setAttribute('src', `cc-${type}.svg`)
+}
+
+globalThis.setCardType = setCardType
