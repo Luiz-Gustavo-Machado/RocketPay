78764ba196a4c2d1005ac6355e454f847b62d0f6
Luiz Gustavo Machado <luiz.gustavo.machado@hotmail.com>
2022-10-20 15:41:36 -0300

    Project finished

--- src/main.js
+++ src/main.js
@@ -18,8 +18,6 @@ function setCardType(type) {
   ccLogo.setAttribute("src", `cc-${type}.svg`)
 }
 
-setCardType("default")
-
 globalThis.setCardType = setCardType
 
 const securityCode = document.querySelector("#security-code")
@@ -70,8 +68,6 @@ const cardNumberPattern = {
       return number.match(item.regex)
     })
 
-    console.log(foundMask)
-
     return foundMask
   },
 }
@@ -90,23 +86,37 @@ const cardHolder = document.querySelector("#card-holder")
 cardHolder.addEventListener("input", () => {
   const ccHolder = document.querySelector(".cc-holder .value")
 
-  ccHolder.innerText = cardHolder.value.length === 0 ? "FULANDO DA SILVA" : cardHolder.value
+  ccHolder.innerText =
+    cardHolder.value.length === 0 ? "FULANO DA SILVA" : cardHolder.value
 })
 
 securityCodeMasked.on("accept", () => {
-
+  updateSecurityCode(securityCodeMasked.value)
 })
 
-ccHolder.innerText = cardHolder.value.length === 0 ? "FULANO DA SILVA" : cardHolder.value
-
-
-securityCodeMasked.on("accept", () => {
-  updateSecurityCode(securityCodeMasked.value);
-})
-
-function updateSecurityCode(code){
+function updateSecurityCode(code) {
   const ccSecutiry = document.querySelector(".cc-security .value")
-  
+
   ccSecutiry.innerText = code.length === 0 ? "123" : code
 }
 
+cardNumberMasked.on("accept", () => {
+  const cardType = cardNumberMasked.masked.currentMask.cardtype
+  setCardType(cardType)
+  updateCardNumber(cardNumberMasked.value)
+})
+
+function updateCardNumber(number) {
+  const ccNumber = document.querySelector(".cc-number")
+
+  ccNumber.innerText = number.length === 0 ? "1234 5678 9012 3456" : number
+}
+
+expirationDateMasked.on("accept", () => {
+  updateExpirationDate(expirationDateMasked.value)
+})
+
+function updateExpirationDate(date) {
+  const ccExpiration = document.querySelector(".cc-extra .value")
+  ccExpiration.innerText = date.length === 0 ? "02/32" : date
+}
